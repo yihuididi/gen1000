592dd30665e5a781780e2291c2839c29fb1740b3
yihuididi <tohyihui11@yahoo.com>
2025-01-14 15:02:14 +0800

    Deploy firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHyUIu9NLqjsaVFq7KhcVEJ3tG8MEKFJ0",
+  authDomain: "gen1000.firebaseapp.com",
+  projectId: "gen1000",
+  storageBucket: "gen1000.firebasestorage.app",
+  messagingSenderId: "382359209764",
+  appId: "1:382359209764:web:98fbe3af2747558f2015b1",
+  measurementId: "G-F07NE6G64H"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
